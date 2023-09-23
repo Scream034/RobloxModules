7689755d47c10f82e2cba086339560630d97707b
paralax <kostgs034@gmail.com>
2023-09-23 20:14:31 +0500

    Fix position

--- Modules/Utils/BasePart.lua
+++ Modules/Utils/BasePart.lua
@@ -6,20 +6,21 @@ local module = {}
 -- Получить позицию по направлению нормали
 --- Пример: UtilsBasePart.GetPositionOnNormal(Parent.CFrame, Parent.Size, Enum.NormalId.Left)
 function module.GetPositionOnNormal(originCframe: CFrame, originSize: Vector3, normalId: Enum.NormalId, position: Vector3?): Vector3
-	local result = position or Vector3.zero
+	position = position or Vector3.zero
+	local result = Vector3.zero
 	
 	if normalId == Enum.NormalId.Right then
-		result = originCframe:PointToWorldSpace(Vector3.new(originSize.X, 0, 0))
+		result = originCframe:PointToWorldSpace(Vector3.new(originSize.X, 0, 0) + position)
 	elseif normalId == Enum.NormalId.Left then
-		result = originCframe:PointToWorldSpace(Vector3.new(-originSize.X, 0, 0))
+		result = originCframe:PointToWorldSpace(Vector3.new(-originSize.X, 0, 0) + position)
 	elseif normalId == Enum.NormalId.Front then
-		result = originCframe:PointToWorldSpace(Vector3.new(0, 0, -originSize.Z))
+		result = originCframe:PointToWorldSpace(Vector3.new(0, 0, -originSize.Z) + position)
 	elseif normalId == Enum.NormalId.Back then
-		result = originCframe:PointToWorldSpace(Vector3.new(0, 0, originSize.Z))
+		result = originCframe:PointToWorldSpace(Vector3.new(0, 0, originSize.Z) + position)
 	elseif normalId == Enum.NormalId.Top then
-		result = originCframe:PointToWorldSpace(Vector3.new(0, originSize.Y, 0))
+		result = originCframe:PointToWorldSpace(Vector3.new(0, originSize.Y, 0) + position)
 	elseif normalId == Enum.NormalId.Bottom then
-		result = originCframe:PointToWorldSpace(Vector3.new(0, -originSize.Y, 0))
+		result = originCframe:PointToWorldSpace(Vector3.new(0, -originSize.Y, 0) + position)
 	end
 	
 	return result
