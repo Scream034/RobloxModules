c7c4b5a00f74dbff203cb629a3356d7bd6274135
paralax <kostgs034@gmail.com>
2023-10-08 15:02:29 +0500

    Update method

--- Modules/Utils/Player.lua
+++ Modules/Utils/Player.lua
@@ -7,10 +7,17 @@ local Module = {}
 
 --- Получает игрока, связанного с указанным партом, на основе имени партa.
 --- @param part BasePart: Парт, для которого ищем игрока.
---- @param name string: (Опционально) Имя партa, чтобы проверить соответствие.
+--- @param methodFindName boolean?: (Опционально) искать ли соответсвия в строке или полностью по ней.
+--- @param name string?: (Опционально) Имя партa, чтобы проверить соответствие.
 --- @return Player?: Игрок, связанный с указанным партом, или nil, если игрок не найден.
-function Module.GetPlayerByPart(part: BasePart, name: string?): Player?
-	if name and part.Name ~= name then return end
+function Module.GetPlayerByPart(part: BasePart, methodFindName: boolean?, name: string?): Player?
+	if name then
+		if methodFindName then
+			if part.Name:find(name) then return end
+		else
+			if part.Name ~= name then return end
+		end
+	end
 
 	local Character = part.Parent
 	if not Character or not Character:IsA("Model") then return end
