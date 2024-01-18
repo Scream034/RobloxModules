d2bfea7e886b4e12d2e4cff04e76be5d78307d97
paralax <kostgs034@gmail.com>
2024-01-19 01:22:00 +0500

    Add new method

--- Modules/Helper/General.lua
+++ Modules/Helper/General.lua
@@ -5,9 +5,24 @@ local PlayersService = game:GetService("Players")
 
 local Module = {}
 
+--- Получает все элементы по типу
+--- @param from Instance: Объект, в котором будет поиск
+--- @param typeIs string: Тип нужных объектов
+--- @return {}?:  Массив всех найденных объектов
+function Module.GetAllElementsOfType(from: Instance, typeIs: string)
+    local elements = {}
+    for _, element in pairs(from:GetDescendants()) do
+        if element:IsA(typeIs) then
+            table.insert(elements, element)
+        end
+	end
+	
+	return elements
+end
+
 --- Копирует все дочерние элементы из одного объекта в другой.
---- @param From Instance: Объект, из которого копируются дочерние элементы.
---- @param To Instance: Объект, в который копируются дочерние элементы.
+--- @param from Instance: Объект, из которого копируются дочерние элементы.
+--- @param to Instance: Объект, в который копируются дочерние элементы.
 function Module.CopyChildren(from: Instance, to: Instance): ()
 	for _, Child in ipairs(from:GetChildren()) do
 		local Clone = Child:Clone()
