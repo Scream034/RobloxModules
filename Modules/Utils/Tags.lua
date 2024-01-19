c63ae7d201e3f862ef92a79ffee9a9ed3a9ec24f
paralax <kostgs034@gmail.com>
2024-01-19 18:17:00 +0500

    Fixes

--- Modules/Utils/Tags.lua
+++ Modules/Utils/Tags.lua
@@ -4,39 +4,48 @@
 
 local CollectionService = game:GetService("CollectionService")
 
-
 local Module = {}
 
 --- Инициализация модуля
 function Module.Initialize()
-	Module.Tags = {}
-end
+	local self = {}	
+	
+	--- Добавить новый тег и зарегистрировать его
+	--- @param string tagName: Имя тега
+	--- @param function registerFn: Имя функции регистрации тега
+	function self:Add(tagName: string, registerFn: "function", removeFn: "function"?)
+		removeFn = removeFn or function()
+			self[tagName] = nil
+		end
+		
+		self[tagName] = {}
+		table.insert(self[tagName], tagName)
+		table.insert(self[tagName], registerFn)
+		table.insert(self[tagName], removeFn)
+	end
+	
+	--- Удалить тег
+	--- @param string tagName: Имя тега
+	function self:Remove(tagName: string)
+		self[tagName][3]()
+	end
 
---- Добавить новый тег и зарегистрировать его
---- @param string tagName: Имя тега
---- @param function handleFn: Имя тега
---- @param function initFn: Имя тега
-function Module:Add(tagName: string, handleFn, initFn)
-	Module.Tags[tagName] = {}
-	Module.Tags[tagName].Name = tagName
-	Module.Tags[tagName].HandleFn = handleFn
-	Module.Tags[tagName].InitFn = initFn
-end
-
---- Удалить тег
---- @param string tagName: Имя тега
-function Module:Remove(tagName: string)
-	Module.Tags[tagName] = nil
-end
-
---- Обновить тег, точнее инициализировать функции указанные у тегов. вызывается в конце или после Add()
---- @param string tagName: Имя тега
-function Module:Update()
-	for _, Tag in pairs(Module.Tags) do
-		for _, Object in pairs(CollectionService:GetTagged(Tag.Name)) do
-			Tag.InitFn(Object)
+	--- Обновить тег, точнее инициализировать функции указанные у тегов. вызывается в конце или после Add()
+	--- @param string tagName: Имя тега
+	function self:Update()
+		for _, Tag in pairs(self) do
+			-- Чтобы не было лишних ошибок
+			local succes = pcall(function(...) if Tag[1] ~= nil then return end end)
+			if not succes then continue end
+			
+			for _, instance in pairs(CollectionService:GetTagged(Tag[1])) do
+				Tag[2]()
+			end
 		end
 	end
+
+	
+	return self
 end
 
 return Module
